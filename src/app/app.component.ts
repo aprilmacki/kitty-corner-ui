25f86107b5d1d32f86c6b91b6e4ad3012f33e07f
April <aprilmack@posteo.net>
2025-03-11 12:53:37 -0500

    Create basic page layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    RouterLink,
+    RouterOutlet
+  ],
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'kitty-corner-ui';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {FeedComponent} from './feed/feed.component';
+import {ProfileComponent} from './profile/profile.component';
+import {SettingsComponent} from './settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: FeedComponent
+  },
+  {
+    path: 'users/:username/profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent
+  }
+];
