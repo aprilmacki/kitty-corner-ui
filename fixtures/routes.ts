cdd084e7d6e9b821c9877172ec3fdc1729f212fc
April <aprilmack@posteo.net>
2025-03-21 15:30:55 -0500

    Use fixture server for get user profile endpoint

--- fixtures/routes.ts
+++ fixtures/routes.ts
@@ -1,32 +1,40 @@
 import * as express from 'express';
+import {UserProfileDto} from '../src/app/services/kitty-corner-api/dtos/user.dto';
+import {GetPostsDto} from '../src/app/services/kitty-corner-api/dtos/posts.dto';
 
 export const router: express.Router = express.Router();
 
-interface TestResponses {
-  success: {
-    status: number,
-    response: {},
-  },
-  failures: [
-    {
-      status: number,
-      response: {}
-    }
-  ]
+interface TestResponses<Dto> {
+  [status: number]: Dto | ErrorResponse
 }
 
-function setResponse(res: express.Response, testResponses: TestResponses, isSuccessful: boolean, failureIndex?: number) {
-  if (isSuccessful) {
-    res.status(testResponses.success.status)
-    res.json(testResponses.success.response);
-  } else {
-    res.status(testResponses.failures[failureIndex!].status)
-    res.json(testResponses.failures[failureIndex!].response);
-  }
+interface ErrorResponse {
+  title: string,
+  status: number,
+  detail: string,
+  path: string
+}
+
+function setResponse(res: express.Response, testResponses: TestResponses<GetPostsDto>, status: number) {
+  res.status(status);
+  res.json(testResponses[status])
 }
 
 router.get('/api/posts/', (req: express.Request, res: express.Response) => {
-  const testResponses: TestResponses = require('./responses/get-posts.json');
-  setResponse(res, testResponses, true);
+  const testResponses: TestResponses<GetPostsDto> = require('./responses/get-posts.json');
+  res.status(200);
+  res.json(testResponses[200])
 });
 
+router.get('/api/users/:username/profile', (req: express.Request, res: express.Response) => {
+  const testData: Map<string, UserProfileDto> = new Map(Object.entries(require('./responses/get-user-profile-data.json')));
+  const testResponses: TestResponses<UserProfileDto> = require('./responses/get-user-profile.json');
+
+  if (testData.has(req.params["username"])) {
+    res.status(200);
+    res.json(testData.get(req.params["username"]));
+  } else {
+    res.status(404);
+    res.json(testResponses[404]);
+  }
+});
--- src/app/services/kitty-corner-api/kitty-corner-api.client.ts
+++ src/app/services/kitty-corner-api/kitty-corner-api.client.ts
@@ -32,58 +32,6 @@ export class KittyCornerApiClient {
   }
 
   getUserProfile(username: string): Observable<UserProfileDto> {
-    if (username === 'aprilmack') {
-      return of({
-        username: 'aprilmack',
-        name: 'April',
-        pronouns: 'she/her',
-        age: 27,
-        location: 'Chicago, IL',
-        joinedAtEpochSeconds: new Date('2025-02-01T08:30-06:00').getSeconds(),
-        totalPosts: 1
-      } as UserProfileDto)
-    } else if (username === 'minathecat') {
-      return of({
-        username: 'minathecat',
-        name: 'Mina',
-        pronouns: 'she/her',
-        age: 4,
-        location: 'Chicago, IL',
-        joinedAtEpochSeconds: new Date('2025-01-23T09:32-06:00').getSeconds(),
-        totalPosts: 154
-      } as UserProfileDto);
-    } else if (username === 'tequila_sunset') {
-      return of({
-        username: 'tequila_sunset',
-        name: 'Harry',
-        pronouns: 'he/him',
-        age: 42,
-        location: 'Revachol, Le Caillou',
-        joinedAtEpochSeconds: new Date('2025-01-25T14:02-06:00').getSeconds(),
-        totalPosts: 2
-      } as UserProfileDto);
-    } else if (username === 'misato') {
-      return of({
-        username: 'misato',
-        name: 'Misato',
-        pronouns: 'she/her',
-        age: 29,
-        location: 'Tokyo 3',
-        joinedAtEpochSeconds: new Date('2025-01-04T18:24-06:00').getSeconds(),
-        totalPosts: 1
-      })
-    } else if (username === 'vincent_van_dough') {
-      return of({
-        username: 'vincent_van_dough',
-        name: 'Kenny',
-        pronouns: 'they/them',
-        age: 32,
-        location: 'Chicago, IL',
-        joinedAtEpochSeconds: new Date('2025-01-06T14:54-06:00').getSeconds(),
-        totalPosts: 3
-      })
-    } else {
-      return of();
-    }
+    return this.httpClient.get<UserProfileDto>(`/api/users/${username}/profile`);
   }
 }
