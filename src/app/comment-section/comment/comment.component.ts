06fbf6484c59e284a2751acb8ccb9d0547d59ffb
April <aprilmack@posteo.net>
2025-03-26 14:42:28 -0500

    Add comment component

--- src/app/comment-section/comment/comment.component.ts
+++ src/app/comment-section/comment/comment.component.ts
@@ -1,14 +1,30 @@
 import {Component, Input} from '@angular/core';
 import {CommentModel} from '../../services/kitty-corner-api/models/comment.model';
+import {DatePipe, DecimalPipe, NgOptimizedImage} from '@angular/common';
+import {MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-comment',
-  imports: [],
+  imports: [
+    DatePipe,
+    NgOptimizedImage,
+    MatIcon,
+    MatIconButton,
+  ],
   templateUrl: './comment.component.html',
-  styleUrl: './comment.component.css'
+  styleUrl: './comment.component.scss'
 })
 export class CommentComponent {
   @Input() comment!: CommentModel;
 
   constructor() {}
+
+  public toggleLike() {
+
+  }
+
+  public toggleDislike() {
+
+  }
 }
