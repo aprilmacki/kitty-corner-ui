a4cf95f185d07cc9a7dc0b471e357b51ae03d79a
April <aprilmack@posteo.net>
2025-03-30 17:29:51 -0500

    Hide profile photo in posts on profile page

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -24,6 +24,7 @@ import {computeLikeDislikeChange} from '../common/util';
 export class PostComponent {
   post = input.required<PostModel>();
   showCommentButton = input<boolean>(true);
+  showPhoto = input<boolean>(true);
 
   constructor(private kittyCornerClient: KittyCornerApiClient) {
   }
